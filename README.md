8b5aa0c5b8d0dc563288ff0fd2179923d401690f
Sasha Babicki <sbabicki@gmail.com>
2015-01-20 20:25:36 -0700

    server accepting multiple client connections + readme update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cmput410lab2
+
+####Command to run program:
+###python lab2.py
+<br >
+Connects to localhost on port 9999
+<br ><br >
+####References Used:
+Article by Silver Moon:
+<br >http://www.binarytides.com/python-socket-programming-tutorial/ 
+<br >Accessed on Jan 20 2015
--- lab2.py
+++ lab2.py
@@ -3,11 +3,11 @@
 # http://www.binarytides.com/python-socket-programming-tutorial/ 
 # Article by Silver Moon
 #
-#
 
 import socket
+from thread import *
 
-HOST = ''
+HOST = "localhost"
 PORT = 9999
 
 # socket is created and socket descriptor is returned
@@ -22,21 +22,36 @@ print("Socket bind complete\n")
 s.listen(10)
 print("Socket now listening\n")
 
-while 1:
+# 1 thread for each connected client
+def client_thread(conn):
 	
+	#conn.send("Welcome message\n")
+	
+	while 1:
+		
+		# recieve data from client
+		data = conn.recv(1024)
+		reply = data[:-2] + " Sasha\r\n"
+		if not data:
+			break
+		
+		# send reply if data is recieved	
+		conn.sendall(reply)
+	
+	# close the connection with that client
+	conn.close()
+	
+while 1:
+
 	# accept connection (blocking call)
 	conn, addr = s.accept()
-	
+
 	#display client information
 	print 'Connected with ' + addr[0] + ':' + str(addr[1])
-
-	data = conn.recv(1024)
-	reply = "OK..."+data
 	
-	# if (data != None)
-	if not data:
-		break
-	conn.sendall(reply)
+	# starts new thread
+	# args -> 1) function to be run, 2) tuple of args for that function
+	start_new_thread(client_thread, (conn,))
 
-conn.close()
+# close the socket	
 s.close()
